edcae8a7a71686c44197bdbc9caf0cd79ef773a6
Bridget <brturnbul@gmail.com>
2018-03-29 16:49:00 +0100

    introduced more burgers but needs refactoring

--- game/app.js
+++ game/app.js
@@ -1,16 +1,15 @@
 $(() =>{
   var score = 0;
   const $sloth = $('#sloth');
-  const $burger = $('.burger');
-  // const $burger2 = $('.burger2');
-  // const $burger3 = $('.burger3');
+  const $burger1 = $('#b1');
+  const $burger2 = $('#b2');
+  const $burger3 = $('#b3');
 
-  // const $burgerOffset2 = $burger2.offset();
-  // const $burgerOffset3 = $burger3.offset();
   setInterval(function(){
     const $slothOffset = $sloth.offset();
-    const $burgerOffset = $burger.offset();
-
+    const $burgerOffset1 = $burger1.offset();
+    const $burgerOffset2 = $burger2.offset();
+    const $burgerOffset3 = $burger3.offset();
 
     document.onkeydown = function(e) {
       switch (e.keyCode) {
@@ -23,16 +22,40 @@ $(() =>{
       }
     };
 
-    if ($burgerOffset.left < $slothOffset.left + $sloth.width() &&
-    $burgerOffset.left + $burger.width() > $slothOffset.left &&
-    $burgerOffset.top < $slothOffset.top + $sloth.height() &&
-    $burger.height() + $burgerOffset.top > $slothOffset.top) {
-        score += 1;
-        $burger.hide();
+    if ($burgerOffset1.left < $slothOffset.left + $sloth.width() &&
+    $burgerOffset1.left + $burger1.width() > $slothOffset.left &&
+    $burgerOffset1.top < $slothOffset.top + $sloth.height() &&
+    $burger1.height() + $burgerOffset1.top > $slothOffset.top) {
+      score += 1;
+      $burger1.hide();
       setTimeout(function (){
-          $burger.show();
-        },1000);
-      console.log(score);
+        $burger1.show();
+      },1000);
+      $('#playerScore').text(score);
+    }
+
+    if ($burgerOffset2.left < $slothOffset.left + $sloth.width() &&
+    $burgerOffset2.left + $burger2.width() > $slothOffset.left &&
+    $burgerOffset2.top < $slothOffset.top + $sloth.height() &&
+    $burger2.height() + $burgerOffset2.top > $slothOffset.top) {
+      score += 1;
+      $burger2.hide();
+      setTimeout(function (){
+        $burger2.show();
+      },1000);
+      $('#playerScore').text(score);
+    }
+
+    if ($burgerOffset3.left < $slothOffset.left + $sloth.width() &&
+    $burgerOffset3.left + $burger3.width() > $slothOffset.left &&
+    $burgerOffset3.top < $slothOffset.top + $sloth.height() &&
+    $burger3.height() + $burgerOffset3.top > $slothOffset.top) {
+      score += 1;
+      $burger3.hide();
+      setTimeout(function (){
+        $burger3.show();
+      },1000);
+      $('#playerScore').text(score);
     }
 
 }, 10);
