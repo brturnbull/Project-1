a8652aeeb21cd1c2fca138541d47e535d153672e
Bridget <brturnbul@gmail.com>
2018-04-05 15:59:28 +0100

    added background music and game sounds

--- game/app.js
+++ game/app.js
@@ -1,41 +1,46 @@
 $(() =>{
 
-  $('#modalStart').show();
-  $('#modalLeaderboard').show();
-  $('.score').hide();
-
   let score = 0;
   let $currentPlayerName;
-  const scores = {};
   let currentPlayerScore;
 
-  const $modalGame = $('#modalGame')
+
+  const audio1 = new Audio('./sounds/ding.mp3');
+  const audio2 = new Audio('./sounds/lose-life.mp3');
+  const audio3 = new Audio('./sounds/sloth-island.mp3');
+  const scores = {};
+  const $modalGame = $('#modalGame');
   const $sloth = $('#sloth');
   const $burgers = $('.burger'); // all burgers
   const $carrots = $('.carrot'); // all sloths
-  const $modal = $('.modal');
+  const $modals = $('.modal');
+  const $score = $('.score');
+  const $countdown = $('.countdown');
+  const $modalStart = $('#modalStart')
+  const $input = $('.inputName');
 
+  audio3.play();
+  $('#modalLeaderboard').show();
+  $modalStart.show();
+  $score.hide();
   $burgers.hide();
   $carrots.hide();
 
   function startGame() {
-    $('.score').show();
+    $score.show();
     setTimeout(function (){
       $modalGame.show();
       $('.playerScore1').text(score);
       currentPlayerScore = score;
-      console.log(currentPlayerScore);
       $burgers.hide();
       $carrots.hide();
       clearInterval();
       localStorage.setItem($currentPlayerName, currentPlayerScore);
       scores[$currentPlayerName] = (currentPlayerScore);
-      console.log(scores);
     }, 5000);
   }
   // Countdown timer - starts at 4, decreases by 1 every  second and hide when
   // it gets to zero
-
   let timeIsRunning = false;
   const $startGameBtn = $('.btn-start');
   function startTimer() {
@@ -46,10 +51,10 @@ $(() =>{
     let $counter = 4;
     const $interval = setInterval(function() {
       $counter--;
-      $('.countdown').text($counter);
+      $countdown.text($counter);
       if ($counter === 0) {
         clearInterval($interval);
-        $('.countdown').hide();
+        $countdown.hide();
         $burgers.show();
         $carrots.show();
         startGame();
@@ -62,14 +67,20 @@ $(() =>{
   $startGameBtn.on('click', function() {
     // if ($('.inputName').val() === '') {
     //   alert('Please enter a valid name');
-      // event.preventDefault()
+    // event.preventDefault()
     // };
-    $('#modalStart').hide();
+    $modalStart.hide();
     timeIsRunning = true;
     startTimer();
   });
 
-  // SLOTH MOVING ON KEY DOWN
+  window.onclick = function(event) {
+    if (event.target !== modal) {
+
+    }
+}
+
+  //-------------------------SLOTH MOVEMENT-------------------------------------
 
   setInterval(function(){
     const $slothOffset = $sloth.offset();
@@ -84,8 +95,7 @@ $(() =>{
           break;
       }
     };
-
-    // CARROTS COLLISION
+    //-------------------------------COLLISIONS---------------------------------
 
     for (let i = 0; i < $burgers.length; i++) {
       if ($($burgers[i]).offset().left < $slothOffset.left + $sloth.width() &&
@@ -93,21 +103,14 @@ $(() =>{
       $($burgers[i]).offset().top < $slothOffset.top + $sloth.height() &&
       $($burgers[i]).height() + $($burgers[i]).offset().top > $slothOffset.top) {
         score += 10;
+        audio1.play();
         $($burgers[i]).hide();
         setTimeout(function (){
           $($burgers[i]).show();
         },1000);
         $('#playerScore').text(score);
-        setTimeout(function(){
-          $('#plus10').fadeIn(100);
-        }, 0);
-        setTimeout(function(){
-          $('#plus10').fadeOut(100);
-        }, 300);
       }
     }
-
-    // CARROTS COLLISION
 
     for (let j = 0; j < $carrots.length; j++) {
       if ($($carrots[j]).offset().left < $slothOffset.left + $sloth.width() &&
@@ -115,6 +118,7 @@ $(() =>{
       $($carrots[j]).offset().top < $slothOffset.top + $sloth.height() &&
       $($carrots[j]).height() + $($carrots[j]).offset().top > $slothOffset.top) {
         score-=5;
+        audio2.play();
         $($carrots[j]).hide();
         setTimeout(function (){
           $($carrots[j]).show();
@@ -124,47 +128,45 @@ $(() =>{
     }
   }, 10);
 
-  // If player chooses to play again; push their score to the array for leaderboard and refresh values
-
+  // If player chooses to play again; reload page (so as to refresh leaderboard)
   $('#playAgain').on('click', () => {
     location.reload();
-    // $('.countdown').show();
-    // console.log(scores);
-    // score=0;
-    // $('#playerScore').text(score);
-    // $timeIsRunning = true;
-    // startTimer();
   });
 
   // -------------------------- Leaderboard -----------------------------------
 
+  // Split local storage object into keys and values to convert scores to numbers
+  // due to localStorage only returning strings (NaN)
   var keys = Object.keys(localStorage);
   var values = Object.values(localStorage).map(Number);
+
+  // Declare new object to sort values now that numbers have been converted
   var result = {};
 
+  // creating a new object with the keys and values matches back up
   keys.forEach((key, i) => result[key] = values[i]);
 
+  // creating a two dimensional array for key, value to sort
   var sortable = [];
   for (var players in result) {
     sortable.push([players, result[players]]);
   }
 
+  // sorting the new 2D array by the second value in each array (the score)
   sortable.sort(function(a, b) {
     return b[1] - a[1];
   });
 
   // INSERT TOP 5 SCORES ON LEADERBOARD - can use a loop
-
+  //
+  // for (let i = 0; i < $highScores.length; i++) {
+  //   $highScores.text(sortable[i]);
+  // }
+  //
   $('.hs1').text(sortable[0]);
   $('.hs2').text(sortable[1]);
   $('.hs3').text(sortable[2]);
   $('.hs4').text(sortable[3]);
   $('.hs5').text(sortable[4]);
-
-//   window.onclick = function(event) {
-//     if (event.target !== $modals) {
-//       $modals.hide();
-//     }
-// }
 
 });
